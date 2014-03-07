fda60b0c803c57d348a2544c068e4d47c11d7eff
Richard <rmedina@CLT-IT-RM.local>
2014-03-07 10:49:01 -1000

    minor changes to prod.txt

--- ggvproject/settings/prod.py
+++ ggvproject/settings/prod.py
@@ -8,24 +8,11 @@ DEBUG = False
 TEMPLATE_DEBUG = False
 
 DATABASES = {
-    'default': {
-        # 'ENGINE':'django.db.backends.postgresql_psycopg2',
-        # 'NAME': 'localdb',
-        # 'USER': 'djangodbuser',
-        # 'PASSWORD': '1',
-        # 'HOST': 'localhost',
-        # 'PORT': '5432',
-    }
+    'default': { }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(PROJECT_DIR, 'db.sqlite3'),
-#     }
-# }
 
-DATABASES['default'] =  dj_database_url.config() #default='postgres://djangodbuser:1@localhost/localdb'
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')

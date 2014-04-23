33f23d06355846bf8be136687d3e4dd728d40bc5
Richard <rmedina@CLT-IT-RM.local>
2014-04-22 21:02:10 -1000

    working on settings

--- ggvproject/settings/prod.py
+++ ggvproject/settings/prod.py
@@ -1,17 +1,7 @@
 # settings/prod.py - configured for Heroku deployment
 
-from .base import *
+# Parse database configuration from $DATABASE_URL
 import dj_database_url
-
-DEBUG = False
-
-TEMPLATE_DEBUG = False
-
-DATABASES = {
-    'default': { }
-}
-
-
 DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
@@ -20,3 +10,12 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
--- ggvproject/wsgi.py
+++ ggvproject/wsgi.py
@@ -11,5 +11,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ggvproject.settings.prod")
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-# application = get_wsgi_application()
 application = Cling(get_wsgi_application())

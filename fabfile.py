f6881c6f3fe43d3413ac3a6fdf63d762a7431d64
r@m <richmedina@gmail.com>
2014-07-15 15:16:11 -1000

    fabfile committed this, not me.

--- fabfile.py
+++ fabfile.py
@@ -4,4 +4,7 @@ def push_changes(message='fabfile committed this, not me.'):
     local("git add -A && git commit -m '%s'" % message)
     local("git push origin master")
 
+def deploy():
+    pass
+
 

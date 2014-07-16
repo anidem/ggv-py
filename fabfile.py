badeda37309613fa8ea748b41dc885b83c90ad4c
r@m <richmedina@gmail.com>
2014-07-15 15:11:45 -1000

    message from the terminal

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,10 @@
 from fabric.api import local
 
-def push_changes():
-    local("git add -A && git commit -m 'fabfile default' ")
+def hello(name="world"):
+    print("Hello %s!" % name)
+
+def push_changes(message='fabfile committed this, not me.'):
+    local("git add -A && git commit -m '%s'" % message)
     local("git push origin master")
 
 

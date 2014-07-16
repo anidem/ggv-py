41cac01f7b7eb7bdae7631ce18cfdd06efb81925
r@m <richmedina@gmail.com>
2014-07-16 04:17:23 -1000

    fabfile committed this, not me.

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,7 @@
 from __future__ import with_statement
 from fabric.api import local, settings, abort, run, cd
 from fabric.contrib.console import confirm
+import time
 
 def push_changes(message='fabfile committed this, not me.'):
     local("git add -A && git commit -m '%s'" % message)
@@ -9,11 +10,22 @@ def push_changes(message='fabfile committed this, not me.'):
 def deploy():
     code_dir = '/Users/rmedina/test-deploy'
     with settings(warn_only=True):
-        if run("test -d %s" % code_dir).failed:
+        if run("cd %s" % code_dir).failed:
             run("git clone https://github.com/anidem/ggv-py.git %s" % code_dir)
     with cd(code_dir):
+        ts = time.strftime('%Y%m%d')
+        run('cd ..')
+        run('tar cvf ggv-py-%s.tar %s' % (ts, code_dir))
+        run('cd %s' % code_dir)
         run('git pull origin master')
         run('touch ggvproject/wsgi.py')
-        
+
+def prompt():
+    if not confirm("You are about to push changes to the repository to a production environment. Are you sure you want to do this?"):
+        abort("Aborting at user request.")
+    ts = time.strftime('%Y%m%d')
+    print(ts)
+
+
 
 

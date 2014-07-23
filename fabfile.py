3996ce344794df6ee83ace479a93d89b81d6c146
r@m <richmedina@gmail.com>
2014-07-23 04:30:01 -1000

    fabfile committed with this message, not me.

--- fabfile.py
+++ fabfile.py
@@ -1,51 +1,32 @@
 from __future__ import with_statement
 from fabric.api import local, settings, abort, run, cd
 from fabric.contrib.console import confirm
+from fabric.operations import prompt
 import time
 
-def push_changes(message='fabfile committed this, not me.'):
+def push_changes(message='fabfile committed with this message, not me.'):
     local("git add -A && git commit -m '%s'" % message)
     local("git push origin master")
 
-def deploy():
-    with settings(warn_only=True):
-        
-        if run("cd %s" % code_dir).failed:
+def deploy(remote_dir):
+    with settings(warn_only=True):        
+        if run("cd %s" % remote_dir).failed:
             run("git clone https://github.com/anidem/ggv-py.git %s" % code_dir)
 
-    with cd(backup_dir):
+    with cd(remote_dir):
         ts = time.strftime('%Y%m%d%H%M%S')
-        run('tar cvf ggv-py-%s.tar %s' % (ts, code_dir))
-    
-    with cd(code_dir):
-        run('git pull origin master')
-        run('touch ggvproject/wsgi.py')
-        run('tree')
-
-def test_deploy(proj_dir, pythonhome):
-    run('uname -a')
-    run('ls -latr')
-
-    code_dir = proj_dir
-    
-    with settings(warn_only=True):
-        if run("cd %s" % code_dir).failed:
-            run("git clone https://github.com/anidem/ggv-py.git %s" % code_dir)
-
-    with cd(pythonhome):
-        ts = time.strftime('%Y%m%d%H%M%S')
-        run('sudo tar cvf test-%s.tar %s' % (ts, code_dir))
-    
-    with cd(code_dir):
+        run('cd ..')
+        run('sudo tar cvf ggv-py-%s.tar %s' % (ts, remote_dir))
         run('sudo git pull origin master')
         run('sudo touch ggvproject/wsgi.py')
-        run('tree')
+        run('sudo ls -l')
+        run('sudo python manage.py collectstatic --settings=ggvproject.settings.prod')
 
-def prompt():
+def start_deploy():
     if not confirm("You are about to push changes to the repository to a production environment. Are you sure you want to do this?"):
         abort("Aborting at user request.")
-    ts = time.strftime('%Y%m%d')
-    print(ts)
+    rd = prompt('Please specify remote directory: ')
+    deploy(remote_dir=rd)
 
 
 

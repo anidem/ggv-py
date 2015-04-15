962f6a5158f4ca750e64ef6e44e67206b0da48af
r@m <richmedina@gmail.com>
2015-04-14 22:43:27 -1000

    fabfile committed with this message, not me.

--- questions/views.py
+++ questions/views.py
@@ -226,7 +226,7 @@ class QuestionResponseView(LoginRequiredMixin, AccessRequiredMixin, CourseContex
             context['formula'] = '/media/pdf/eng-formula-page.pdf'
         elif self.worksheet.lesson.id == 5:  # Spanish math lesson
             context['calculator'] = '/media/pdf/eng/ti-30xs-calculator-english.pdf'
-            context['formula'] = '/media/pdf/span-formula-page.pdf.pdf'
+            context['formula'] = '/media/pdf/span-formula-page.pdf'
 
         actionstr = 'access-question-' + current_question.get_question_type()
         ActivityLog(

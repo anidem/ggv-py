76f5e3fb08f58a241d1068479d95582c90dab83a
r@m <richmedina@gmail.com>
2015-05-15 01:52:31 -1000

    user progress tables added and adjusted roster manager

--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,7 @@
 from operator import attrgetter
+from collections import OrderedDict, namedtuple
+from datetime import datetime
+
 from django.views.generic import DetailView
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
@@ -7,6 +10,7 @@ from braces.views import LoginRequiredMixin
 
 from core.mixins import AccessRequiredMixin, PrivelegedAccessMixin, RestrictedAccessZoneMixin
 from questions.models import QuestionSet
+from slidestacks.models import SlideStack
 from .models import Course
 
 
@@ -70,25 +74,97 @@ class UserManageView(LoginRequiredMixin, AccessRequiredMixin, RestrictedAccessZo
         """
         (activitylog entry, score)
         """
-        activity = []
-        for i in user.activitylog.all().extra({'day': 'date(timestamp)'}):
+        activity = OrderedDict()  # {'day': {'duration': n, 'activity_list':[dicts]}
+        for i in user.activitylog.all():
+            tkey = datetime.strftime(i.timestamp, '%b-%d-%Y')
+            try:
+                activity[tkey]
+            except:
+                activity[tkey] = []
+
             if i.action == 'completed-worksheet':
                 try:
                     wurl = i.message.split('/')
                     course = Course.objects.get(slug=wurl[2])
                     worksheet = QuestionSet.objects.get(pk=wurl[4])
                     report_url = reverse('worksheet_user_report', args=[course.slug, worksheet.id, user.id])
+
+                    activity[tkey].append({'activity': i, 'report_url': report_url, 'worksheet': worksheet})
+                except:
+                    pass  # malformed log message. proceed silently...
+
+            elif i.action == 'access-presentation' or i.action == 'access-worksheet':
+                activity[tkey].append({'activity': i, 'report_url': i.message, 'worksheet': None, 'score': None})
+
+            else:
+                activity[tkey].append({'activity': i, 'report_url': i.message, 'worksheet': None, 'score': None})
+
+        context['student_user'] = user
+        context['activity_log'] = activity
+
+        return context
+
+
+class UserProgressView(LoginRequiredMixin, AccessRequiredMixin, RestrictedAccessZoneMixin, PrivelegedAccessMixin, DetailView):
+    model = Course
+    template_name = 'course_user_progress.html'
+    slug_url_kwarg = 'crs_slug'
+    access_object = None
+
+    def get(self, request, *args, **kwargs):
+        # course = Course.objects.get(slug=self.kwargs['crs_slug'])
+        return super(UserProgressView, self).get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(UserProgressView, self).get_context_data(**kwargs)
+        user = User.objects.get(pk=self.kwargs['user'])
+
+        """
+        (activitylog entry, score)
+        """
+        activity = OrderedDict()
+        for i in user.activitylog.all():
+
+
+            activity_info = []
+            try:
+                activity_info = [j for j in i.message.split('/ggv/')[1].split('/')]
+
+            except:
+                pass
+
+            activity_dict = None
+            if i.action == 'completed-worksheet':
+                try:
+                    crs, ws_id = activity_info[0], activity_info[2]
+                    course = Course.objects.get(slug=crs)
+
+                    worksheet = QuestionSet.objects.get(pk=ws_id)
+                    report_url = reverse('worksheet_user_report', args=[course.slug, worksheet.id, user.id])
                     report = worksheet.get_user_responses(user, worksheet.get_ordered_question_list(), course)
                     score = report['grade']
 
-                    activity.append({'activity': i, 'report_url': report_url, 'worksheet': worksheet, 'score': score})
+                    activity_dict = {'activity': i, 'access_time': None, 'completed_time': i.timestamp, 'report_url': report_url, 'course': course,  'content': worksheet, 'score': score}
                 except:
                     pass  # malformed log message. proceed silently...
-            elif i.action == 'access-presentation' or i.action == 'access-worksheet':
-                activity.append({'activity': i, 'report_url': i.message, 'worksheet': None, 'score': None})
 
-            else:
-                activity.append({'activity': i, 'report_url': i.message, 'worksheet': None, 'score': None})
+            elif i.action == 'access-presentation':
+                try:
+                    crs, stack_id = activity_info[0], activity_info[2]
+                    stack = SlideStack.objects.get(pk=stack_id)
+                    course = Course.objects.get(slug=crs)
+                    activity_dict = {'activity': i, 'access_time': i.timestamp, 'completed_time': None, 'report_url': i.message, 'course': self.get_object(), 'content': stack, 'score': None}
+                except:
+                    pass
+
+            if activity_dict:
+
+                tkey = datetime.strftime(i.timestamp, '%b-%d-%Y')
+                try:
+                    activity[tkey].append(activity_dict)
+                except:
+                    activity[tkey] = []
+                    activity[tkey].append(activity_dict)
 
         context['student_user'] = user
         context['activity_log'] = activity

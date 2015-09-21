f83335cbf47625b66b2abc3651760222e5f0c9aa
r@m <richmedina@gmail.com>
2015-09-20 18:13:34 -0600

    fabfile committed with this message, not me.

--- core/views.py
+++ core/views.py
@@ -22,7 +22,7 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        context['site_message'] = SiteMessage.objects.get(url_context='/')
+        # context['site_message'] = SiteMessage.objects.get(url_context='/')
         return context
 
 

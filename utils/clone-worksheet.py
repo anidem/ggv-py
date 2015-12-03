1483258e54eb7bf7e702552d25d3b532fee711e1
r@m <richmedina@gmail.com>
2015-12-02 22:24:43 -0700

    worksheet clone and adjustment to lesson to display the display order property

---
+++ utils/clone-worksheet.py
@@ -0,0 +1,27 @@
+from questions.models import *
+from lessons.models import *
+
+lesson = Lesson.objects.get(pk=5)
+section = Section.objects.get(pk=215)
+
+ws_ids = []
+
+for w in ws_ids:
+    worksheet = QuestionSet.objects.get(pk=w)
+    questions = worksheet.get_ordered_question_list()
+    worksheet.pk = None
+    worksheet.save()
+    worksheet.lesson = lesson
+    worksheet.section = section
+    worksheet.save()
+    for q in questions:
+        options = q.options.all()
+        q.pk = None
+        q.save()
+        q.question_set = worksheet
+        q.save()
+        for o in options:
+            o.pk = None
+            o.save()
+            o.question = q
+            o.save()

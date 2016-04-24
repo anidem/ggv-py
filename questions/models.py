9dd59bd7aaa47f7caf1def20cca4851e02fd9472
r@m <richmedina@gmail.com>
2016-04-24 12:04:02 -0600

    cleanup

--- questions/models.py
+++ questions/models.py
@@ -473,7 +473,7 @@ class QuestionResponse(TimeStampedModel):
             status = UserWorksheetStatus.objects.filter(completed_worksheet=self.content_object.question_set).get(user=self.user)
             status.update_score()
         except Exception as e:
-            print 'model',e
+            # print 'model',e
             pass  # status object null, user has not completed all questions.
 
         

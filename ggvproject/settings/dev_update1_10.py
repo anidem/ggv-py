d87d38b8e98114e28b797aa31f1d318efa3e3ff8
r@m <richmedina@gmail.com>
2016-10-03 22:33:47 -0600

    update adjusts

--- ggvproject/settings/dev_update1_10.py
+++ ggvproject/settings/dev_update1_10.py
@@ -10,7 +10,7 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ggvdbruby',
+        'NAME': 'ggvdbruby-update',
         'USER': 'postgres',
         'PASSWORD': '1',
         'HOST': '127.0.0.1',
@@ -78,5 +78,5 @@ LOGOUT_URL = '/logout/'
 
 SESSION_SECURITY_INSECURE = True
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
-SESSION_SECURITY_WARN_AFTER =  10 # 5 mins to move mouse
-SESSION_SECURITY_EXPIRE_AFTER = 15 # expire after 30 mins
+SESSION_SECURITY_WARN_AFTER =  5000 # 5 mins to move mouse
+SESSION_SECURITY_EXPIRE_AFTER = 5001 # expire after 30 mins

00e48e47883c8113cb3276c1ef1347e56f3649ac
r@m <richmedina@gmail.com>
2016-12-30 13:57:05 -1000

    fix email to staff when managers not assigned

--- core/emails.py
+++ core/emails.py
@@ -341,7 +341,11 @@ class SendEmailToManagerActivationRequest(CsrfExemptMixin, LoginRequiredMixin, C
             for i in self.course.manager_list():  # Bypassing user settings to control email messages from ggv system
                 manager_list.append(i.email)
    
-
+            if not manager_list: # if not manager found. send request to staff.
+                staff = User.objects.filter(is_staff=True)
+                for i in staff:
+                    manager_list.append(i.email)
+                    
             course_slugs = self.request.session['user_courses']
             course_titles = ''
             for i in course_slugs:
